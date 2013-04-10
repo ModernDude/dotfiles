34e6fbe973cb1fe470acaea041897df1bd391569
Jeff Sigmon <jeff@jeffsigmon.com>
2013-04-09 19:15:37 -0500

    add ace jump mode

---
+++ dots/.emacs.d/init.el
@@ -0,0 +1,5 @@
+
+(setq custom-file "~/.emacs.d/me/custom.el")
+(when (file-exists-p custom-file) (load custom-file))
+
+(load "~/.emacs.d/me/packages.el")
---
+++ dots/.emacs.d/me/packages.el
@@ -0,0 +1,42 @@
+(require 'package)
+
+(add-to-list 'package-archives
+  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(ace-jump-mode)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+
+;; global requires
+
+(require 'cl)
+
+;; init ace jump mode
+(autoload
+  'ace-jump-mode
+  "ace-jump-mode"
+  "Emacs quick move minor mode"
+  t)
+;; you can select the key you prefer to
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+
+;;
+;; enable a more powerful jump back function from ace jump mode
+;;
+(autoload
+  'ace-jump-mode-pop-mark
+  "ace-jump-mode"
+  "Ace jump back:-)"
+  t)
+(eval-after-load "ace-jump-mode"
+  '(ace-jump-mode-enable-mark-sync))
+(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
